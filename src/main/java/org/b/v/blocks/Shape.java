53c2639e66458ae312988c9c1b56dabd39c5b381
bart <bart_voet@telenet.be>
2017-02-01 22:06:09 +0100

    [wip] Simplifying the design

--- src/main/java/org/b/v/blocks/Shape.java
+++ src/main/java/org/b/v/blocks/Shape.java
@@ -4,9 +4,6 @@ public interface Shape {
 	boolean isNeighbour(Shape shape);
 	void draw(ShapePainter painter);
 	
-	//recursief algoritme?
-	//gaat zijn buren af
-	//die gaan hun buren af behalve de aanroepende
-	//als de shapepainter een shape tegenkomt die getekend is maar overlapt klopt er iets nniet
-	
+
+
 }
